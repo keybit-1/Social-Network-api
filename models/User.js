4d27f36a5cd56d5c62c334714b78e7fdad0adc34
keybit-1 <131838974+keybit-1@users.noreply.github.com>
2023-10-19 18:55:05 -0500

    created base files

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Must be a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+module.exports = model('User', UserSchema);
